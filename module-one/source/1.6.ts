b71702dc2e29e5479d0626a188c747c6260087b6
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-02 12:57:47 -0700

    add funct ion

--- module-one/source/1.6.ts
+++ module-one/source/1.6.ts
@@ -8,3 +8,16 @@ function add(num1:number, num2:number = 10){
 add(4, 2)
 
 const addArrow(num1:number, num2:number): number => num1 + num2
+
+// object --> function --> method
+const poorUser = {
+    name: 'tufael',
+    balance: 0,
+    addBalance(balance:number):string{
+        return `My new balance is : ${this.balance + balance}`
+    }
+}
+addArrow(4, 6)
+
+const arr: number[] = [2,4, 6, 8]
+const newArray: number[] = arr.map((elm:number):number => elm*elm)
