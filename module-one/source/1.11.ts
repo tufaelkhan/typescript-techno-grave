0794e133013d72c7b8857e83dc6c6e583e347a16
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-02 23:24:01 -0700

    add ternary

---
+++ module-one/source/1.11.ts
@@ -0,0 +1,11 @@
+{
+// ternary operator || optional chaining || nullish coalesching operator
+const age: number = 15;
+if(age >= 19){
+    console.log('adult');
+}else{
+    console.log('not adult');
+}
+
+
+}
