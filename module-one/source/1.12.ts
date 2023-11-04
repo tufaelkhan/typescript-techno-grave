535ef69012435a555e0bd88c6f8f52ba661b62dc
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-03 18:40:21 -0700

    add nullable type

---
+++ module-one/source/1.12.ts
@@ -0,0 +1,12 @@
+{
+// nullable type
+const searchName = (value:string) =>{
+    if(value){
+        console.log('searching name');
+    }else{
+        console.log('there is nothing to searching');
+    }
+}
+searchName('tufael')
+
+}
