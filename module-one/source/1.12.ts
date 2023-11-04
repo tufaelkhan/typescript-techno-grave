1704f387c1dedb102e325a2e7ac58fb43d861b2c
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-03 19:12:35 -0700

    add module

--- module-one/source/1.12.ts
+++ module-one/source/1.12.ts
@@ -1,12 +1,33 @@
 {
 // nullable type
-const searchName = (value:string) =>{
+const searchName = (value:string | null) =>{
     if(value){
         console.log('searching name');
     }else{
         console.log('there is nothing to searching');
     }
 }
-searchName('tufael')
+searchName(null)
 
+// unknown typeof
+const getSpeedInMeterPerSecond = ( value: unknown) =>{
+    if(typeof value === 'number'){
+        const covertedSpeed = (value*1000)/3600
+        console.log(`the speed is ${covertedSpeed} ms^-1`);
+    }
+    else if( typeof value === 'string'){
+        const [result, unit] = value.split(' ')
+        const covertedSpeed = (parseFloat(result)*1000)/3600
+        console.log(`the speed is ${covertedSpeed} ms^-1`);
+    }else{
+        console.log('wrong input');
+    }
+}
+getSpeedInMeterPerSecond(`1000 kmh^-1`)
+getSpeedInMeterPerSecond(null)
+
+const throwError = (msg:string):never => {
+    throw new Error(msg)
+}
+throwError('error ho ga ya')
 }
