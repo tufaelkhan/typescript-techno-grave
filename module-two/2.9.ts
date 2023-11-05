21dc0b3740f7d9d6583a147e5643a7d269a25be6
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-05 06:52:47 -0800

    add conditional type

--- module-two/2.9.ts
+++ module-two/2.9.ts
@@ -1,5 +1,22 @@
 {
 // conditional type
+type A1 = number
+type B1 = string
+type X1 = A1 extends null ? true : false    // condition type
+type Y1 = A1 extends null ? true : B1 extends undefined ? undefined : any
 
+type Sheikh = {
+    bike: string;
+    car: string;
+    ship: string;
+    plan: string;
+}
+// keyof sheikh 'bike' | 'car' | 'ship'
+
+// car ashe kina / bike ashe kina / tractor ashe kina
+type CheckVehicle<T> = T extends keyof Sheikh? true : false
+
+type HasBike = CheckVehicle<'ship'>
+type HasTractor = CheckVehicle<'tractor'>
 
 }
