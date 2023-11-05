9dcf13c8aa579b80054f78257f8e621f26a4775a
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-04 19:24:03 -0700

    add keyof

--- module-two/2.7.ts
+++ module-two/2.7.ts
@@ -1,4 +1,32 @@
 {
+// generic constraints with keyof operator
+type Vehicle = {
+    bike: string;
+    car: string;
+    ship: string;
+}
+type Owner = 'bike' | "car" | 'ship'
+type Owner2 = keyof Vehicle;
+
+const person1: Owner2 = 'ship'
+
+const getPropertyValue = <X, Y extends keyof X>(obj: X, key: Y) => {
+    return obj[key]
+}
+const user = {
+    name: 'tufael',
+    age: 23,
+    address: 'chandpur',
+}
+const car = {
+    model: 'toyota',
+    year: 23,
+}
+const result1 = getPropertyValue(car, 'model')
+
+
+// obj['age']
+
 
 
 }
