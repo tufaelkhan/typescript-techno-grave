4c5016eaadf9d14879df71be605154ba7817786d
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-07 10:34:19 -0800

    add polymorphism

--- module-three/3.8.ts
+++ module-three/3.8.ts
@@ -25,4 +25,47 @@ const person2 = new Student()
 const person3 = new Developer()
 
 getSleepingHours(person1)
+getSleepingHours(person2)
+getSleepingHours(person3)
+
+
+class Shape{
+    getArea(): number{
+        return 0;
+    }
+}
+class Circle extends Shape{
+    redius: number;
+    constructor(redius: number){
+        super()
+        this.redius = redius;
+    }
+    getArea( ): number {
+        return Math.PI * this.redius * this.redius;
+    }
+}
+
+// height width
+class Ractangle extends Shape{
+    height: number;
+    width: number;
+    constructor(height: number, width: number){
+        super()
+        this.height = height;
+        this.width =  width;
+    }
+    getArea( ): number {
+        return this.height * this.width
+    }
+}
+
+const getShapeArea = (param: Shape) =>{
+    console.log(param.getArea());
+}
+const shape1 = new Shape()
+const shape2 = new Circle(10)
+const shape3 = new Ractangle(10, 20)
+
+getShapeArea(shape3)
+
 }
---
+++ module-three/3.9.ts
@@ -0,0 +1,5 @@
+{
+// abstraction
+
+
+}
