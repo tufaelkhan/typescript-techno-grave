df085b82c8bce89f5bcd76336a77fbeef24e7c4e
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-07 11:22:03 -0800

    add encapsulation

---
+++ module-three/3.10.ts
@@ -0,0 +1,35 @@
+{
+    // access modifiers
+    class BankAccount{
+        public readonly id: number;
+        public name: string;
+        protected _balance: number;
+    
+        constructor(id: number, name: string, balance: number){
+            this.id = id;
+            this.name = name;
+            this._balance = balance;
+        }
+        addDeposit(amount: number){
+            this._balance = this._balance + amount
+        }
+        private getBalance(){
+            return this._balance
+        }
+        getHiddenMethod(){
+            return this.getBalance()
+        }
+    }
+    
+    class StudentAccount extends BankAccount{
+        test(){
+            this._balance
+        }
+    }
+    const goribManusherAcccount = new BankAccount( 123, 'tufael', 30)
+    goribManusherAcccount.addDeposit(40)
+    const myBalance = goribManusherAcccount.getHiddenMethod()
+    console.log(myBalance);
+    
+    
+    }
--- module-three/3.9.ts
+++ module-three/3.9.ts
@@ -1,5 +1,60 @@
 {
-// abstraction
+// abstraction  1. interface    2. abstract
+// idea
+interface Vehicle1{
+    startEngine():void
+    stopEngine():void
+    move():void
+}
+// const vehicle1:Vehicle1 = {
+//     name: 'toyota',
+//     model: 2000,
+// }
+
+// real implementation
+class Car1  implements Vehicle1{
+    startEngine(): void {
+        console.log('I am starting the car engine');
+    }
+    stopEngine(): void {
+        console.log('I am stop the engine');
+    }
+    move(): void {
+        console.log('move the car');
+    }
+    test(){
+        console.log('i am test the engine');
+    }
+}
+
+const toyotaCar = new Car1()
+toyotaCar.startEngine()
+
+// abstract class
+// idea
+abstract class Car2{
+    abstract startEngine(): void 
+    abstract stopEngine(): void
+    abstract move(): void
+    test(){
+        console.log('i am test the engine');
+    }
+}
+
+class ToyotaCar extends Car2{
+    startEngine(): void {
+        console.log('I am starting the car engine');
+    }
+    stopEngine(): void {
+        console.log('I am stopping the car engine');
+    }
+    move(): void {
+        console.log('I am move the car engine');
+    }
+}
+
+const hondacar = new Car2()
+hondacar.startEngine()
 
 
 }
