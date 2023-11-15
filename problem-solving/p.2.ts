f17edc2da79bd82428c11786a2d17667663cd25c
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-14 18:27:46 -0800

    add problem

--- problem-solving/p.2.ts
+++ problem-solving/p.2.ts
@@ -1,4 +1,24 @@
 {
+const keyValueArray: [string, any][] = [
+    ["id", 10],
+    ["name", "habib"],
+    ["marrid", true],
+]
 
+const valueObj = {
+    id: 10,
+    name: 'habib',
+    marrid: true
+}
+function arrayToObject<T extends string, U >(array: [T, U[]]): Record<T, U> {
+    const transformObject = array.reduce((finalObject, [key, value]) =>{
+        if(value.length > 0){
+            finalObject[key] = value[0]; 
+        }
+        return finalObject
+        }, {} as Record<T, U>);
+        return transformObject;
+}
+console.log(arrayToObject(valueObj));
 
 }
